d9ba7373a748a72fcb806fa70d44c91005615c3c
moluzequvit928-star <moluzequvit928@gmail.com>
2026-04-11 20:46:47 +0300

    Add files via upload

--- init_stats_db.php
+++ init_stats_db.php
@@ -1,6 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=futurama;charset=utf8', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once 'db.php';
 
 // История событий (принятия/увольнения)
 $pdo->exec("CREATE TABLE IF NOT EXISTS staff_events (
