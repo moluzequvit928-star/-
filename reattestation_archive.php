b6599e8d1c7ad4f4e4220a310300a4be32c27d14
moluzequvit928-star <moluzequvit928@gmail.com>
2026-04-13 10:28:02 +0300

    Add files via upload

--- reattestation_archive.php
+++ reattestation_archive.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once 'db.php';
+require_once 'user_header.php';
 
 if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
     header('Location: login.php');
