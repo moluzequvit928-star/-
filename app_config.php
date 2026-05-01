b4d5dfd42688ff468bd46270a0796511535ab4f0
moluzequvit928-star <moluzequvit928@gmail.com>
2026-05-01 18:17:24 +0300

    Add files via upload

--- app_config.php
+++ app_config.php
@@ -9,8 +9,8 @@ return [
     'reattestation_gid' => '822458528',
 
     // URL Web App из Google Apps Script.
-    'app_script_webhook_url' => 'https://script.google.com/macros/s/AKfycbx0zka3uWPgFDZu7yG4Tso39Ftcf7-IGMW5wNsiV89blCTUyRCca8XuckfkD55f_Rkmlg/exec',
+    'app_script_webhook_url' => 'https://script.google.com/macros/s/AKfycbwJ14FaYGLVOEzx-SCVg5EwuhcdGi9pNBA4x8kmFvl1hQJRUVNHBnnTITAojS4FrCmWeA/exec',
 
     // Тот же токен, что SECRET_TOKEN в Apps Script.
-    'app_script_webhook_token' => '123',
+    'app_script_webhook_token' => 'futika_2026_q7N4vP2xLm8Kc5Rz',
 ];
