00f7e6b7de5a4a5e955a25f3edc4f62302ca5a20
moluzequvit928-star <moluzequvit928@gmail.com>
2026-05-08 15:31:52 +0300

    Add files via upload

---
+++ app_config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'google_sheet_id' => '1w2r_C3R7kh5CDvlehOHOjd3DPnvCMBQ9SnXZnB6t754',
+    'main_sheet_gid' => '1970062457',
+    'reattestation_gid' => '822458528',
+    'app_script_webhook_url' => 'https://script.google.com/macros/s/AKfycbwJ14FaYGLVOEzx-SCVg5EwuhcdGi9pNBA4x8kmFvl1hQJRUVNHBnnTITAojS4FrCmWeA/exec',
+    'app_script_webhook_token' => 'futika_2026_q7N4vP2xLm8Kc5Rz',
+];
